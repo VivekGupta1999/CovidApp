eee6914faedd7ecef445be1fa9b45b5ce5d753b3
Vandan00007 <patelvandan@icloud.com>
2020-12-16 14:33:03 -0500

    news pages

---
+++ src/TabScreen.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Container, Header, Content,Left, Body, Right, Title, Tab, Tabs } from 'native-base';
+import Tab1 from './screens/tab1';
+import Tab2 from './screens/tab2';
+
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header style={{backgroundColor:'#2b3475'}} hasTabs>
+            <Left/>
+          <Body>
+            <Title style={{color:'white'}}>News App</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs tabBarUnderlineStyle={{backgroundColor:'white'}}>
+          <Tab tabStyle={{backgroundColor:'#2b3475'}} activeTabStyle={{backgroundColor:'#2b3475'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="General">
+            <Tab1 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#2b3475'}} activeTabStyle={{backgroundColor:'#2b3475'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="Covid">
+            <Tab2 />
+          </Tab>
+        
+          
+        </Tabs>
+      </Container>
+    );
+  }
+}
---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'ca';
+export const category = 'everything';
+export const _api_key = 'ef313414f102490a9c17de6cb601e702';
