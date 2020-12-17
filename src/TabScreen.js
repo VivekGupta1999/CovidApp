27b027f3adcf2b666388ddcb5a3901aae7e8eebe
Vivek Gupta <59952831+VivekGupta1999@users.noreply.github.com>
2020-12-17 08:20:27 -0500

    Add files via upload

--- src/TabScreen.js
+++ src/TabScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Header, Content,Left, Body, Right, Title, Tab, Tabs } from 'native-base';
 import Tab1 from './screens/tab1';
 import Tab2 from './screens/tab2';
+import Tab2 from './screens/tab3';
 
 export default class TabsExample extends Component {
   render() {
@@ -20,6 +21,9 @@ export default class TabsExample extends Component {
           </Tab>
           <Tab tabStyle={{backgroundColor:'#2b3475'}} activeTabStyle={{backgroundColor:'#2b3475'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="Covid">
             <Tab2 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:'#2b3475'}} activeTabStyle={{backgroundColor:'#2b3475'}} textStyle={{color:'white'}} activeTextStyle={{color:'white'}} heading="Covid-discussions">
+            <Tab3 />
           </Tab>
         
           
